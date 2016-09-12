174b5969edef9898dcb48055e8c314590275a07f
Ionut <ionut.leustean@outlook.com>
2016-09-12 10:36:19 +0300

    bootstrap@4.0.0-alpha.4

--- client/app.component.ts
+++ client/app.component.ts
@@ -5,6 +5,8 @@ import { DemoComponent } from './imports/demo/demo.component';
 //noinspection TypeScriptCheckImport
 import template from './app.component.html';
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 @Component({
   selector: 'app',
   template,
