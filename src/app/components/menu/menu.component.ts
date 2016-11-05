46b31e3601abd12a0dd2770670f31e4b66b6f81f
Ionut <ionut.leustean@outlook.com>
2016-11-05 22:46:12 +0200

    menu - redesign

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,17 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {
+    Router,
+    NavigationStart
+} from "@angular/router";
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.less']
+    selector: 'app-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.less']
 })
 export class MenuComponent implements OnInit {
 
-  private isCollapsed: boolean = true;
-  constructor() {
-  }
+    private color: string;
+    private background: string;
+    private border: string;
+    private menuState: boolean = false;
 
-  ngOnInit() {
-  }
+    constructor(router: Router) {
 
+
+        this.setLandingPageStyle = this.setLandingPageStyle.bind(this);
+
+        router.events
+            .filter(event => event instanceof NavigationStart)
+            .subscribe(results => {
+                    if (results.url === "/") {
+
+                        this.landingPageInitStyle();
+                        window.addEventListener('scroll', this.setLandingPageStyle);
+
+                    } else {
+                        window.removeEventListener('scroll', this.setLandingPageStyle);
+                        this.defaultStyle();
+                    }
+                }
+            );
+
+
+    }
+
+    ngOnInit() {
+    }
+
+    landingPageInitStyle() {
+        this.border = `0px 0px 5px 0px rgba(0,0,0,0)`;
+        this.color = `rgb(255,255,255)`;
+        this.background = `rgba(255,255,255,0)`;
+    }
+
+    defaultStyle() {
+        this.border = `0px 0px 5px 0px rgba(0,0,0,0.25)`;
+        this.color = `rgb(0,0,0)`;
+        this.background = `rgba(255,255,255,1)`;
+    }
+
+    setLandingPageStyle() {
+        let scrollTop, windowHeight, opacity, saturation;
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+        windowHeight = window.outerHeight - 200;
+        opacity = Math.min(scrollTop, windowHeight) / windowHeight;
+        saturation = 255 * (1 - opacity);
+        this.border = `0px 0px 5px 0px rgba(0,0,0,${opacity / 4})`;
+        this.color = `rgb( ${saturation}, ${saturation}, ${saturation})`;
+        this.background = `rgba(255,255,255,${opacity})`;
+    }
+
+    toggleMenu() {
+        this.menuState = !this.menuState;
+    }
 }
+
+
