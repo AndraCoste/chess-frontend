e84688ceed51d0b499c7f00d31be979891048581
DenisTDR <denis.tdr@gmail.com>
2016-11-06 21:26:25 +0200

    register layout

--- src/app/modules/register/register.component.ts
+++ src/app/modules/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+const EmailRegex = "^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup;
+  private loading:boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      'firstName': ['', Validators.required],
+      'lastName': ['', Validators.required],
+      'email' : ['', [Validators.required, Validators.pattern(EmailRegex)]],
+      'phone': ['', Validators.required],
+      'birthDate': ['', Validators.required],
+      'university': ['', Validators.required],
+      'legitimated' : false,
+    })
+  }
 
   ngOnInit() {
+  }
+
+  submitForm(form:FormGroup) {
+    console.log(form.value);
   }
 
 }
