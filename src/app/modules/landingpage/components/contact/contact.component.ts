1666d1004f850f467977829e7429453ff99803de
Erik Blenert <erik.blenert@ligaac.ro>
2016-11-10 23:56:08 +0200

    Cleanup.

--- src/app/modules/landingpage/components/contact/contact.component.ts
+++ src/app/modules/landingpage/components/contact/contact.component.ts
@@ -2,9 +2,6 @@
 import {Component, Inject} from "@angular/core";
 import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
 import {ArticleService, ARTICLE_SERVICE} from "../../../../services/article.service";
-import ArticleDataObject from "../../../../model/article-data-object";
-
-const EmailRegex = "^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
 
 @Component({
     selector: 'contact',
@@ -12,21 +9,6 @@ const EmailRegex = "^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA
     styleUrls: ['./contact.style.less']
 })
 export class Contact {
-
-    private data: ArticleDataObject = {
-        title: null,
-        author: null,
-        body: null,
-        image: null,
-        selector: null
-    };
-
-    // private model = {
-    //     name: null,
-    //     email: null,
-    //     subject: null,
-    //     message: null
-    // };
 
     private send = false;
 
@@ -36,27 +18,24 @@ export class Contact {
         @Inject(ARTICLE_SERVICE) private articleService: ArticleService,
         private formBuilder: FormBuilder
     ) {
-        articleService.getArticle('contact').subscribe(
-            data => {this.data = data; console.log(data)},
-            err => {throw new Error('articleService Error: ')}
-        )
     }
 
-    onSubmit() {
-        
-    }
     submitForm(form:FormGroup){
         this.articleService.postContact(form.value).subscribe(
-            data => this.send = true,
+            data => {
+                this.send = true;
+                form.reset();
+            },
             err => {
                 this.send = false
                 throw Error("contact form error") }
         )
     }
+
     ngOnInit() {
         this.form = this.formBuilder.group({
             'name': ['', Validators.required],
-            'email': ['', Validators.required, Validators.pattern(EmailRegex)],
+            'email': ['', Validators.required],
             'subject': ['', Validators.required],
             'message': ['', Validators.required]
         })
