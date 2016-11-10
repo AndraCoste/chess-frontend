4583f615e44d84f5d5171b568d52d5d98a488dc5
Erik Blenert <erik.blenert@ligaac.ro>
2016-11-10 22:54:10 +0200

    Refactored to reactive forms.

--- src/app/modules/landingpage/components/contact/contact.component.ts
+++ src/app/modules/landingpage/components/contact/contact.component.ts
@@ -1,8 +1,10 @@
 
 import {Component, Inject} from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
 import {ArticleService, ARTICLE_SERVICE} from "../../../../services/article.service";
 import ArticleDataObject from "../../../../model/article-data-object";
+
+const EmailRegex = "^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
 
 @Component({
     selector: 'contact',
@@ -19,32 +21,45 @@ export class Contact {
         selector: null
     };
 
-    private model = {
-        name: null,
-        email: null,
-        subject: null,
-        message: null
-    };
+    // private model = {
+    //     name: null,
+    //     email: null,
+    //     subject: null,
+    //     message: null
+    // };
 
     private send = false;
 
-    constructor(@Inject(ARTICLE_SERVICE) private articleService: ArticleService) {
+    form: FormGroup;
 
-
-
+    constructor(
+        @Inject(ARTICLE_SERVICE) private articleService: ArticleService,
+        private formBuilder: FormBuilder
+    ) {
         articleService.getArticle('contact').subscribe(
-            data => {this.data = data},
+            data => {this.data = data; console.log(data)},
             err => {throw new Error('articleService Error: ')}
         )
     }
 
     onSubmit() {
-        this.articleService.postContact(this.model).subscribe(
+        
+    }
+    submitForm(form:FormGroup){
+        this.articleService.postContact(form.value).subscribe(
             data => this.send = true,
             err => {
                 this.send = false
                 throw Error("contact form error") }
         )
+    }
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            'name': ['', Validators.required],
+            'email': ['', Validators.required, Validators.pattern(EmailRegex)],
+            'subject': ['', Validators.required],
+            'message': ['', Validators.required]
+        })
     }
 
 
